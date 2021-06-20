bdc29a4cff405dbea660b864749cdeef0abd6c7e
dazacher <dazacher2000@yahoo.com>
2021-06-20 07:48:58 -0700

    Adding a logging service using dependency injection and a provider.

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,9 +1,8 @@
+import { Injectable } from "@angular/core";
+@Injectable()
 
-
-export class LoggingService() {
- logStatusChange(status: string) {
+export class LoggingService {
+    logStatusChange(status: string) {
      console.log('A server status changed, new status: ' + status);
- }
-    
-
+    }
 }
